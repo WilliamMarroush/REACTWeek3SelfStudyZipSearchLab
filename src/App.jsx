f00b125e84ca2ca49ca8e837d7c6a6af57d02cab
William Marroush <William.Marroush@csi.cuny.edu>
2025-04-02 12:37:08 -0400

    Setting a state variable to take in the zip code, and show it on the city component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
 import { useState } from "react";
 import "./App.css";
 
-function City(props) {
-  return <div>This is the City component</div>;
+function City({searchZip}) {
+  return(
+    <>
+      <p> Entered Zip: {searchZip} </p>
+    </>
+  );
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+function ZipSearchField({onZipChange}) {
+  const handleChange = (e) =>{
+    onZipChange(e.target.value);
+  }
+  const handleEnter = (e) =>{
+    if (e.key === 'Enter'){
+      onZipChange(e.target.value);
+    }
+  };
+  return (
+  <>
+    <h2>Zip Code:</h2>
+    <input id = "zip" type="text"
+    onKeyPress = {handleEnter}
+    maxLength={5}
+    />
+    <button
+    onClick ={()=> onZipChange(document.getElementById('zip').value)}
+    >Search</button>
+  </>
+  );
 }
 
 function App() {
+  const [enteredZip, setEnteredZip] = useState(null);
+  const updateZip = (zip)=>{
+    setEnteredZip(zip);
+  }
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
+        <ZipSearchField onZipChange={updateZip} />
         <div>
-          <City />
-          <City />
+          <City searchZip = {enteredZip}/>
         </div>
       </div>
     </div>
